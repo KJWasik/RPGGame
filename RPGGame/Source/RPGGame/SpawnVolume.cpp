4c3611121162a290ea3aa0f632277eeaa06e9273
Krzysztof Wasik <krzysztofwasik1988@wp.pl>
2019-11-14 22:07:25 +0000

    Working on spawn volume. C++ class SpawnVolume added.

---
+++ RPGGame/Source/RPGGame/SpawnVolume.cpp
@@ -0,0 +1,39 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "SpawnVolume.h"
+#include "Components/BoxComponent.h"
+#include "Kismet/KismetMathLibrary.h"
+
+// Sets default values
+ASpawnVolume::ASpawnVolume()
+{
+ 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+	SpawningBox = CreateDefaultSubobject<UBoxComponent>(TEXT("SpawningBox"));
+
+}
+
+// Called when the game starts or when spawned
+void ASpawnVolume::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+// Called every frame
+void ASpawnVolume::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
+FVector ASpawnVolume::GetSpawnPoint()
+{
+	FVector Extent = SpawningBox->GetScaledBoxExtent();
+	FVector Origin = SpawningBox->GetComponentLocation();
+	FVector Point = UKismetMathLibrary::RandomPointInBoundingBox(Origin, Extent);
+	return Point;
+}
+
---
+++ RPGGame/Source/RPGGame/SpawnVolume.h
@@ -0,0 +1,34 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "SpawnVolume.generated.h"
+
+UCLASS()
+class RPGGAME_API ASpawnVolume : public AActor
+{
+	GENERATED_BODY()
+	
+public:	
+	// Sets default values for this actor's properties
+	ASpawnVolume();
+
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Spawning")
+	class UBoxComponent* SpawningBox;
+
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Spawning")
+	TSubclassOf<class ACritter> PawnToSpawn;
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+public:	
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+
+	UFUNCTION(BlueprintPure, Category = "Spawning")
+	FVector GetSpawnPoint();
+};
